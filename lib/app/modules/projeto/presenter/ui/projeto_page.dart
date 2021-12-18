f67fb97f08b107e518fa092d100bfe18c61c3166
Wagner Andrade <wagnerpbandrade@gmail.com>
2021-12-18 17:18:25 -0300

    Add package material_floating_search_bar

--- lib/app/modules/projeto/presenter/ui/projeto_page.dart
+++ lib/app/modules/projeto/presenter/ui/projeto_page.dart
@@ -1,5 +1,7 @@
 import 'package:forestinv_mobile/app/modules/projeto/constants/ui_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class ProjetoPage extends StatefulWidget {
   @override
@@ -10,11 +12,66 @@ class ProjetoPageState extends State<ProjetoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(UIText.PROJECT_UI_TEXT),
-        centerTitle: true,
+      resizeToAvoidBottomInset: false,
+      drawer: Drawer(
+        child: Container(
+          width: 200,
+        ),
       ),
-      body: Container(),
+      body: buildFloatingSearchBar(),
+    );
+  }
+
+  Widget buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'Pesquisar projetos',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.icon(
+          icon: Icon(Icons.close_rounded),
+          onTap: () => {},
+          showIfOpened: true,
+          showIfClosed: false,
+        )
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
