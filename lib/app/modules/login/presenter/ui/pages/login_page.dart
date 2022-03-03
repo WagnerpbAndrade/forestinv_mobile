41dc77afb13221f06eed09dd0823c018d7381464
Wagner Andrade <wagnerpbandrade@gmail.com>
2022-03-02 21:25:47 -0300

    Removendo classes sem utilidade

--- lib/app/modules/login/presenter/ui/pages/login_page.dart
+++ lib/app/modules/login/presenter/ui/pages/login_page.dart
@@ -1,48 +1,166 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:forestinv_mobile/app/core/constants/colors_const.dart';
+import 'package:forestinv_mobile/app/core/constants/router_const.dart';
+import 'package:forestinv_mobile/app/core/widgets/custom_elevated_button.dart';
+import 'package:forestinv_mobile/app/core/widgets/error_box.dart';
 import 'package:forestinv_mobile/app/modules/login/presenter/output/stores/login_store.dart';
-import 'package:forestinv_mobile/app/modules/login/presenter/ui/widgets/components/google_login_button.dart';
-import 'package:forestinv_mobile/app/modules/login/presenter/ui/widgets/components/login_footer.dart';
-import 'package:forestinv_mobile/app/modules/login/presenter/ui/widgets/components/logo_login.dart';
-import 'package:forestinv_mobile/app/modules/login/presenter/ui/widgets/form_login.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  LoginPageState createState() => LoginPageState();
-}
-
-class LoginPageState extends State<LoginPage> {
-  final LoginStore store = Modular.get();
-  final firebaseAuth = Modular.get<FirebaseAuth>();
-
-  @override
-  void initState() {
-    super.initState();
-    if (firebaseAuth.currentUser != null) {
-      print('User logged: ${firebaseAuth.currentUser}');
-      Modular.to.pushNamed('/projeto');
-    }
-  }
+class LoginPage extends StatelessWidget {
+  final store = Modular.get<LoginStore>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Center(
+      backgroundColor: ColorsConst.primary,
+      appBar: AppBar(
+        title: const Text('Entrar'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 32),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            elevation: 8,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  const LogoLogin(),
-                  //FormLogin(),
-                  const GoogleLoginButton(),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.2,
+                  Text(
+                    'Acessar com e-mail:',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[900],
+                    ),
                   ),
-                  const LoginFooter(),
+                  Observer(
+                    builder: (_) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: ErrorBox(
+                          message: store.error,
+                        ),
+                      );
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 3, bottom: 4, top: 8),
+                    child: Text(
+                      'E-mail',
+                      style: TextStyle(
+                        color: Colors.grey[900],
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Observer(
+                    builder: (_) {
+                      return TextField(
+                        enabled: !store.loading,
+                        decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            isDense: true,
+                            errorText: store.emailError),
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: store.setEmail,
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 3, bottom: 4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Senha',
+                          style: TextStyle(
+                            color: Colors.grey[900],
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        GestureDetector(
+                          child: const Text(
+                            'Esqueceu sua senha?',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: ColorsConst.primary,
+                            ),
+                          ),
+                          onTap: () {},
+                        )
+                      ],
+                    ),
+                  ),
+                  Observer(
+                    builder: (_) {
+                      return TextField(
+                        enabled: !store.loading,
+                        decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            isDense: true,
+                            errorText: store.passwordError),
+                        obscureText: true,
+                        onChanged: store.setPassword,
+                      );
+                    },
+                  ),
+                  Observer(
+                    builder: (_) {
+                      return CustomElevatedButton(
+                        onPressed: store.loginOnPressed,
+                        child: store.loading
+                            ? const CircularProgressIndicator(
+                                valueColor:
+                                    AlwaysStoppedAnimation(Colors.white),
+                              )
+                            : const Text(
+                                'ENTRAR',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                      );
+                    },
+                  ),
+                  const Divider(
+                    color: Colors.grey,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      children: [
+                        const Text(
+                          'Não tem uma conta? ',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Modular.to.pushNamed(RouterConst.SIGN_UP_ROUTER);
+                          },
+                          child: const Text(
+                            'Cadastre-se',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: ColorsConst.primary,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
--- lib/app/modules/login/presenter/ui/widgets/components/google_login_button.dart
+++ lib/app/modules/login/presenter/ui/widgets/components/google_login_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:forestinv_mobile/app/core/constants/colors_const.dart';
-import 'package:forestinv_mobile/app/modules/login/presenter/output/controller/login_controller.dart';
+import 'package:forestinv_mobile/app/modules/login/presenter/output/stores/login_store.dart';
 
 class GoogleLoginButton extends StatefulWidget {
   const GoogleLoginButton({Key? key}) : super(key: key);
@@ -13,33 +13,12 @@ class GoogleLoginButton extends StatefulWidget {
 class _GoogleLoginButtonState extends State<GoogleLoginButton> {
   @override
   Widget build(BuildContext context) {
-    final controller = Modular.get<LoginController>();
+    final store = Modular.get<LoginStore>();
     return SizedBox(
       height: 50,
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () async {
-          final response = await controller.loginWithGoogle();
-          if (!response.ok) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                  response.message ?? '',
-                  style: const TextStyle(
-                    color: ColorsConst.textColorPrimary,
-                  ),
-                ),
-                backgroundColor: ColorsConst.primary,
-                action: SnackBarAction(
-                  textColor: ColorsConst.textColorPrimary,
-                  label: 'Ok',
-                  onPressed: () {},
-                ),
-                duration: const Duration(milliseconds: 1500),
-              ),
-            );
-          }
-        },
+        onPressed: store.loginWithGoogle,
         style: ButtonStyle(
             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
