8caa84f3c6c23187389dfde8cc9c8e3a4c20cf67
Wagner Andrade <wagnerpbandrade@gmail.com>
2022-06-12 01:21:56 -0300

    Travando orientacao do app em modo portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:forestinv_mobile/app/core/configuration/default_firebase_config.dart';
 
@@ -11,5 +11,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseConfig.platformOptions);
   //await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-  runApp(ModularApp(module: AppModule(), child: AppWidget()));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(ModularApp(module: AppModule(), child: AppWidget()));
+  });
 }
